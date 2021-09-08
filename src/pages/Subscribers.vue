95867d9831f19626f6d591648d7feab2d30626cb
Joshua Belarmino <joshua.aproductfit@gmail.com>
2021-09-08 16:28:02 +0800

    subscribe

--- src/pages/Subscribers.vue
+++ src/pages/Subscribers.vue
@@ -1,11 +1,46 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="row">
+      <div class="col-md-5">
+        <q-table
+          title="Subscribers"
+          dense
+          :rows="rows"
+          :columns="columns"
+          row-key="email"
+          :loading="loading"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   // name: 'PageName',
+  data () {
+    return {
+      columns: [
+        { name: 'email', align: 'left', label: 'Email', field: 'email', sortable: true }
+      ],
+      rows: [],
+      loading: false
+    }
+  },
+  methods: {
+    preload () {
+      this.loading = true
+      this.$api.get('/subscribe/getSubscribers', {
+
+      })
+        .then(response => {
+          this.rows = response.data.data
+          this.loading = false
+        })
+    }
+  },
+  created () {
+    this.preload()
+  }
 }
 </script>
